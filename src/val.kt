db8f5f34fa0eea9cdbdb08321f52f5533f93f76e
dienatisa-ctrl <dienatisa@gmail.com>
2025-09-10 17:58:36 +0700

    contoh val saya

---
+++ src/val.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>){
+    var varVariabel: Int
+    varVariabel = 56 //assigning first value to varVariabel
+    println("pemberian nilai pertama pada varVariabel =" +varVariabel)
+    varVariabel = 78// assigning second value to variabel
+    println("pemberian nilai kedua pada varVariabel ="+varVariabel)
+    var phi = 3.14
+    phi = 2.5
+
+}
