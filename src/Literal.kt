c53bcdb2c80c65d92cdb4b2e72084ab230cb96b8
dienatisa-ctrl <dienatisa@gmail.com>
2025-09-10 16:32:57 +0700

    contoh literal

---
+++ src/Literal.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+    var intLiteral = 5
+    var dobleLiteral = .02
+    var stringLiteral = "Hello"
+    var charLiteral = '1'
+    var boolLiteral = true
+    println("intLiteral = " + intLiteral)
+    println("dobleLiteral = " + dobleLiteral)
+    println("stringLiteral = " + stringLiteral)
+    println("charLiteral = " + charLiteral)
+    println("boolLiterall = " + boolLiteral)
+}
