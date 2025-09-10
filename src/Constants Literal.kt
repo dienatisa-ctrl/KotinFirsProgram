65727b9005937a493a75ba3aff570ed88564ff06
dienatisa-ctrl <dienatisa@gmail.com>
2025-09-10 22:08:56 +0700

    contoh Constants Literal saya

---
+++ src/Constants Literal.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+    val floatLiteral: Float= 178.95F //Literal Float
+    println("contoh Literal Float :" + floatLiteral)
+    val oneBillion: Long = 1000000000L //literal long
+    println("contoh literal long :" + oneBillion)
+    val valHeksa = 0x0F //Literal Haksadesimal diawali dengan 0x
+    println("contoh literal Haksadesimal : "+ valHeksa)
+    val valBinary = 0b00001011 //literial binary diawali dengan 0b
+    println("contoh literal Binary :" + valBinary)
+    val notasikonvensional = 123.5e10
+    println("contoh Literal Konvensional Floating point Number :" +notasikonvensional)
+}
